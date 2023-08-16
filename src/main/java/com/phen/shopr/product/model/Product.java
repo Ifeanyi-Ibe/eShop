48fb437d0b021b9fae8dccb9853538f3d417d6d0
Ifeanyi Ibe <phenomenonif@gmail.com>
2023-08-16 16:48:28 +0100

    Add validation

--- src/main/java/com/phen/shopr/product/model/Product.java
+++ src/main/java/com/phen/shopr/product/model/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Data
@@ -18,10 +19,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     private Long category_id;
 
-
+    @NotBlank(message = "Product must have a name")
     private String name;
 
 
